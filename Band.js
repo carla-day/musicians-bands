030696e98751fb9ffae92f349ca8f0b3e121ed5c
carla-day <carladay44@gmail.com>
2022-05-11 00:41:54 -0400

    missing test & associations

--- Band.js
+++ Band.js
@@ -2,7 +2,10 @@ const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
 let Band;
-
+Band = sequelize.define('Bands',{
+    name:Sequelize.STRING,
+    genre: Sequelize.STRING
+})
 module.exports = {
     Band
 };
